d474a4576a442347ec2671f03a3b75302eee8965
Magartej <155970891+Magartej@users.noreply.github.com>
2025-11-01 11:38:29 +0545

    remove

--- backend/src/middleware/auth.middleware.js
+++
@@ -1,9 +0,0 @@
-function isAuthenticated(req, res, next) {
-  if (req.session?.userId) {
-    next();
-  } else {
-    res.status(401).json({ error: "Unauthorized" });
-  }
-}
-
-module.exports = isAuthenticated;
